c61cb697920c6093a68be05f1c461aca6657fdc6
christinajorgensen <christina.jorgensen@apply.agency>
2023-05-24 10:55:27 +0200

    html attr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "da",
+      },
+      title: "Kalender overblik",
+      charset: "utf-8",
+    },
+  },
+});
